a3710a06be96f5d603c6b9cbd57a54f1c20e65d7
Jack Anderson <phrygian.jack@gmail.com>
2017-02-20 14:20:01 +0000

    Added message validation test

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -3,5 +3,21 @@ class GramsController < ApplicationController
 	end
 
 	def new
+		@gram = Gram.new
 	end	
+
+	def create
+		@gram = Gram.create(gram_params)
+
+		if @gram.valid?
+			redirect_to root_path
+		else 
+			render :new, status: :unprocessable_entity
+		end
+	end
+
+private
+	def gram_params
+		params.require(:gram).permit(:message)
+	end
 end
