83854e7971f6c215c318275e182a5ad98d74196d
Jack Anderson <phrygian.jack@gmail.com>
2017-02-23 15:51:59 +0000

    Refactored comment tests

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,10 @@
 class CommentsController < ApplicationController
-
+	before_action :authenticate_user!, only: [:create]
 	def create
 		@gram = Gram.find_by_id(params[:gram_id])
-		if current_user
-			if @gram.present? 
-				@gram.comments.create(comment_params.merge(user: current_user))
-				redirect_to root_path
-			else
-				return render :new, status: :not_found
-			end
-		else
-			redirect_to new_user_session_path
-		end
-
+		return render_not_found if @gram.blank?
+		@gram.comments.create(comment_params.merge(user: current_user))
+		redirect_to root_path
 	end
 
 private
